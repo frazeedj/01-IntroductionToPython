ba61cb82673cac39d5b7d9f184a6cdb2d39b3503
frazeedj <frazeedj@rose-hulman.edu>
2018-08-30 09:28:39 -0400

    ok

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -32,16 +32,16 @@ import math as mt
 print(77 + mt.cos(2.75))
 
 ###############################################################################
-# TODO: 3.
+# DONE: 3.
 #   Immediately below this _TODO_, write code that computes and prints:
 #      the square root of ((41 * 88) + (4 * the cosine of 2))
 #   Use as few or as many intermediate names as you feel appropriate.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-print(mt.sqrt((41 * 88)+ (4 * mt.cos(2))))
+print(mt.sqrt((41 * 88) + (4 * mt.cos(2))))
 
 ###############################################################################
-# TODO: 4.
+# DONE: 4.
 #   Immediately below this _TODO_,
 #   write code that computes the square root of 2 in two ways:
 #     - By using the   math.sqrt   function.
@@ -49,10 +49,10 @@ print(mt.sqrt((41 * 88)+ (4 * mt.cos(2))))
 #   Print both of the expressions that you write.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-
-
+print(mt.sqrt(2))
+print(2**.5)
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number")
@@ -91,3 +91,16 @@ print(mt.sqrt((41 * 88)+ (4 * mt.cos(2))))
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
 ###############################################################################
+print(type(3.14))
+print(type("hello"))
+print(type('hello'))
+print(type("a b c"))
+print(type(3+3))
+print(type("3" + "3"))
+print(type(2 ** 100))
+print(type(2.0 ** 100))
+print(type(mt.sin(8)))
+print(type(mt.sin))
+print(type(print))
+print(type(mt))
+print(type('mt'))
